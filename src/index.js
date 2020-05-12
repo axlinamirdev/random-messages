90c4cb70baf65b5f4b7f74d3d8b8b5d6607676ec
Axlin Developer <axlina@example.com>
2020-05-12 09:18:50 -0400

    Configuracion

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const messages = [
+	"Oscar",
+	"Carolina",
+	"Paulina",
+	"Teresa",
+	"Ana",
+	"Nicolay",
+	"Yesica",
+	"Diego",
+	"Laura"
+];
+
+const randomMsg = () => {
+	const message = messages[Math.floor(Math.random() * messages.length)];
+	console.log(message);
+}
+
+module.exports = { randomMsg };
